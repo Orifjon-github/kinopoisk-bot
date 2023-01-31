926e30f078238ca8f136bc1b500c6d351da1e06d
Orifjon-github <avazbekoripov94@gmail.com>
2023-01-31 18:53:52 +0500

    find type

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -12,37 +12,15 @@ class TelegramBotController extends Controller
  public function start() {
      $telegram = new Telegram(env('TELEGRAM_BOT_TOKEN'));
      $result = $telegram->getData();
-     $chat_id = $result['message'] ['chat']['id'];
-     $text = $result['message'] ['text'];
-     if ($text == '/start') {
-         Step::set_step(Step::STEP_START);
+     $chat_id = $telegram->ChatID();
+     $type = $result['message'];
+     if (array_key_exists('text', $type)) {
+         $content = array('chat_id' => $chat_id, 'text' => 'text');
+     } elseif (array_key_exists('video', $type)) {
+         $content = array('chat_id' => $chat_id, 'text' => 'video');
      } else {
-         Step::set_step(Step::STEP_SEARCH);
+         $content = array('chat_id' => $chat_id, 'text' => 'text va video emas');
      }
-
-     switch (Step::get_step()) {
-         case Step::STEP_START:
-             $content = array('chat_id' => $chat_id, 'text' => 'Assalomu alaykum. Botimizga xush kelibsiz!!! Qidirayotgan kinoyingizni bir zumda topamiz. Shunchaki qidiring...');
-             $telegram->sendMessage($content);
-             break;
-         case Step::STEP_SEARCH:
-             $movies = Movies::all();
-             $result = array();
-             $no = 1;
-             foreach ($movies as $movie) {
-                 $result[$no] = $movie->name;
-                 $no++;
-                 foreach ($result as $key => $value) {
-                     $text = ''.$key.'. '.$value;
-                 }
-             }
-             $content = array('chat_id' => $chat_id, 'text' => $text);
-             $telegram->sendMessage($content);
-             break;
-         default:
-             $content = array('chat_id' => $chat_id, 'text' => 'switch default');
-             $telegram->sendMessage($content);
-
-     }
+     $telegram->sendMessage($content);
  }
 }
