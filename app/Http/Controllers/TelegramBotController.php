e3ef61b78385da987037ce4f4f2dc9c693512e0e
Orifjon-github <avazbekoripov94@gmail.com>
2024-03-27 13:57:13 +0500

    test inline keyboard

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -9,14 +9,15 @@ class TelegramBotController extends Controller
         $telegram = new Telegram(env('TELEGRAM_BOT_TOKEN'));
         $result = $telegram->getData();
         $chat_id = $telegram->ChatID();
-        $type = $result['message'];
-        if (array_key_exists('text', $type)) {
-            $content = array('chat_id' => $chat_id, 'text' => 'text');
-        } elseif (array_key_exists('video', $type)) {
-            $content = array('chat_id' => $chat_id, 'text' => 'video');
-        } else {
-            $content = array('chat_id' => $chat_id, 'text' => 'text va video emas');
-        }
+        $option = array(
+            //First row
+            array($telegram->buildInlineKeyBoardButton("Button 1", $url= "https://link1.com"), $telegram->buildInlineKeyBoardButton("Button 2", $url= "https://link2.com")),
+            //Second row
+            array($telegram->buildInlineKeyBoardButton("Button 3", $url= "https://link3.com"), $telegram->buildInlineKeyBoardButton("Button 4", $url= "https://link4.com"), $telegram->buildInlineKeyBoardButton("Button 5", $url= "https://link5.com")),
+            //Third row
+            array($telegram->buildInlineKeyBoardButton("Button 6", $url= "https://link6.com")) );
+        $keyb = $telegram->buildInlineKeyBoard($option);
+        $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "This is a Keyboard Test");
         $telegram->sendMessage($content);
     }
 }
