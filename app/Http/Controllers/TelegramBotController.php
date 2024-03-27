69973876eb18e558c0c79495dcf7080b5b862161
Orifjon-github <avazbekoripov94@gmail.com>
2024-03-27 16:30:04 +0500

    test inline keyboard

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -17,19 +17,28 @@ class TelegramBotController extends Controller
         if (!empty($callback_query)) {
             $callback_data = $telegram->Callback_Data();
             if ($callback_data == 'check') {
-                $content = ["chat_id" => "-1001987932786", "user_id" => $user_id];
-                $result = $telegram->getChatMember($content);
-                $telegram->sendMessage(['chat_id' => $chat_id, 'text' => json_encode($result, JSON_UNESCAPED_UNICODE) . " a"]);
+                $content1 = ["chat_id" => "-1001987932786", "user_id" => $user_id];
+                $result1 = $telegram->getChatMember($content1);
+                $content2 = ["chat_id" => "-1001987932786", "user_id" => $user_id];
+                $result2 = $telegram->getChatMember($content2);
+                if ($result1["ok"] && $result2['ok']) {
+                    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Botdan bemalol foydalanishingiz mumkin"]);
+                    exit();
+                }
+                $this->error($telegram, $chat_id);
             }
         } else {
-            $option = array(
-                array($telegram->buildInlineKeyBoardButton("1 - kanal", $url= "https://t.me/+Z9QnOES4AkphNGYy"), $telegram->buildInlineKeyBoardButton("2 - kanal", $url= "https://t.me/orifjon_orifov"), $telegram->buildInlineKeyBoardButton("Tekshirish ✅", "", "check"))
-            );
-
-            $keyb = $telegram->buildInlineKeyBoard($option);
-            $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "❌ Kechirasiz botimizdan foydalanishdan oldin ushbu kanallarga a'zo bo'lishingiz kerak.");
-            $telegram->sendMessage($content);
+            $this->error($telegram, $chat_id);
         }
+    }
 
+    private function error(Telegram $telegram, $chat_id) {
+        $option = array(
+            array($telegram->buildInlineKeyBoardButton("1 - kanal", $url= "https://t.me/+Z9QnOES4AkphNGYy"), $telegram->buildInlineKeyBoardButton("2 - kanal", $url= "https://t.me/orifjon_orifov"), $telegram->buildInlineKeyBoardButton("Tekshirish ✅", "", "check"))
+        );
+
+        $keyb = $telegram->buildInlineKeyBoard($option);
+        $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "❌ Kechirasiz botimizdan foydalanishdan oldin ushbu kanallarga a'zo bo'lishingiz kerak.");
+        $telegram->sendMessage($content);
     }
 }
