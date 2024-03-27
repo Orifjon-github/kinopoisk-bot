f4a91876d64924601cb4287981ed1a45d9259f98
Orifjon-github <avazbekoripov94@gmail.com>
2024-03-27 13:26:41 +0500

    tayyor

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -2,25 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\Step;
-use App\Models\Movies;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Telegram;
-
 class TelegramBotController extends Controller
 {
- public function start() {
-     $telegram = new Telegram(env('TELEGRAM_BOT_TOKEN'));
-     $result = $telegram->getData();
-     $chat_id = $telegram->ChatID();
-     $type = $result['message'];
-     if (array_key_exists('text', $type)) {
-         $content = array('chat_id' => $chat_id, 'text' => 'text');
-     } elseif (array_key_exists('video', $type)) {
-         $content = array('chat_id' => $chat_id, 'text' => 'video');
-     } else {
-         $content = array('chat_id' => $chat_id, 'text' => 'text va video emas');
-     }
-     $telegram->sendMessage($content);
- }
+    public function start()
+    {
+        $telegram = new Telegram(env('TELEGRAM_BOT_TOKEN'));
+        $result = $telegram->getData();
+        $chat_id = $telegram->ChatID();
+        $type = $result['message'];
+        if (array_key_exists('text', $type)) {
+            $content = array('chat_id' => $chat_id, 'text' => 'text');
+        } elseif (array_key_exists('video', $type)) {
+            $content = array('chat_id' => $chat_id, 'text' => 'video');
+        } else {
+            $content = array('chat_id' => $chat_id, 'text' => 'text va video emas');
+        }
+        $telegram->sendMessage($content);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/', [\App\Http\Controllers\TelegramBotController::class, 'start']);
+Route::post('/start', [\App\Http\Controllers\TelegramBotController::class, 'start']);
 
