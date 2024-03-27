352490b59f3762a15b779095fcc3fa40a3623f5c
Orifjon-github <avazbekoripov94@gmail.com>
2024-03-27 14:23:24 +0500

    test inline keyboard

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -12,13 +12,13 @@ class TelegramBotController extends Controller
         $result = $telegram->getData();
         $chat_id = $telegram->ChatID();
         $option = array(
-            //First row
-            array($telegram->buildInlineKeyBoardButton("1 - kanal", $url= "https://t.me/+Z9QnOES4AkphNGYy"), $telegram->buildInlineKeyBoardButton("2 - kanal", $url= "https://t.me/orifjon_orifov"), $telegram->buildInlineKeyBoardButton("Tekshirish ✅", $callback_data = "Tekshirish")),
+            array($telegram->buildInlineKeyBoardButton("1 - kanal", $url= "https://t.me/+Z9QnOES4AkphNGYy"), $telegram->buildInlineKeyBoardButton("2 - kanal", $url= "https://t.me/orifjon_orifov"), $telegram->buildInlineKeyBoardButton("Tekshirish ✅", $callback_data = "Tekshirish"))
         );
 
         $keyb = $telegram->buildInlineKeyBoard($option);
         $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "❌ Kechirasiz botimizdan foydalanishdan oldin ushbu kanallarga a'zo bo'lishingiz kerak.");
         $telegram->sendMessage($content);
-        $telegram->sendMessage($result);
+        $content = array('chat_id' => $chat_id, 'text' => json_encode($result, JSON_UNESCAPED_UNICODE));
+        $telegram->sendMessage($content);
     }
 }
