90f65046665e14412dc2856dff5f60a3a84fb7ea
Orifjon-github <avazbekoripov94@gmail.com>
2024-04-09 12:00:02 +0500

    test bot working

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -42,40 +42,64 @@ class TelegramBotController extends Controller
             }
             exit();
         }
+
+        if (isset($data['message']['text']) && $data['message']['text'] == '/start') {
+            if ($this->check($telegram, $chat_id, $user_id)) {
+                $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Xush kelibsiz! Botdan to'liq foydalanishingiz mumkin! Kino kodini yuboring!"]);
+                exit();
+            }
+        }
         $callback_query = $telegram->Callback_Query();
         if (!empty($callback_query)) {
             $callback_data = $telegram->Callback_Data();
             if ($callback_data == 'check') {
-                $content1 = ["chat_id" => "-1001513289865", "user_id" => $user_id];
-                $result1 = $telegram->getChatMember($content1);
-                $content2 = ["chat_id" => "-1001987932786", "user_id" => $user_id];
-                $result2 = $telegram->getChatMember($content2);
-
-                if ($result1['ok'] && $result2['ok']) {
-                    if ($result1['result']['status'] == "member" && $result2['result']['status'] == "member") {
-                        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Xush kelibsiz! Botdan to'liq foydalanishingiz mumkin!"]);
-                    } else {
-                        $this->error($telegram, $chat_id);
-                    }
-                } else {
-                    $this->error($telegram, $chat_id);
+                if ($this->check($telegram, $chat_id, $user_id)) {
+                    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Xush kelibsiz! Botdan to'liq foydalanishingiz mumkin! Kino kodini yuboring!"]);
+                    exit();
                 }
             }
-        } else {
-            $this->error($telegram, $chat_id);
+        }
+        if (isset($data['message']['text']) && is_numeric($data['message']['text']) && $data['message']['text'] < 1000) {
+            if ($this->check($telegram, $chat_id, $user_id)) {
+                $movie = Movies::where('code', $data['message']['text'])->first() ?? null;
+                if ($movie) {
+                    $telegram->sendVideo(['chat_id' => $chat_id, 'video' => $movie->file_id, 'caption' => $movie->caption]);
+                } else {
+                    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Ushbu kod bo'yicha hech qanday kino topilmadi ❌"]);
+                }
+                exit();
+            }
         }
     }
 
     private function error(Telegram $telegram, $chat_id, $answer = false)
     {
         $option = array(
-            array($telegram->buildInlineKeyBoardButton("1 - kanal (private)", $url = "https://t.me/+Z9QnOES4AkphNGYy")),
-            array($telegram->buildInlineKeyBoardButton("2 - kanal (public)", $url = "https://t.me/orifjon_orifov")),
+            array($telegram->buildInlineKeyBoardButton("1 - kanal", $url = "https://t.me/+Z9QnOES4AkphNGYy")),
+            array($telegram->buildInlineKeyBoardButton("2 - kanal", $url = "https://t.me/orifjon_orifov")),
             array($telegram->buildInlineKeyBoardButton("Tekshirish ✅", "", "check"))
         );
 
         $keyb = $telegram->buildInlineKeyBoard($option);
         $content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "❌ Kechirasiz botimizdan foydalanishdan oldin ushbu kanallarga a'zo bo'lishingiz kerak.");
         $telegram->sendMessage($content);
+        exit();
+    }
+
+    private function check(Telegram $telegram, $chat_id, $user_id) {
+        $content1 = ["chat_id" => "-1001513289865", "user_id" => $user_id];
+        $result1 = $telegram->getChatMember($content1);
+        $content2 = ["chat_id" => "-1001987932786", "user_id" => $user_id];
+        $result2 = $telegram->getChatMember($content2);
+
+        if ($result1['ok'] && $result2['ok']) {
+            if ($result1['result']['status'] == "member" && $result2['result']['status'] == "member") {
+                return true;
+            } else {
+                $this->error($telegram, $chat_id);
+            }
+        } else {
+            $this->error($telegram, $chat_id);
+        }
     }
 }
